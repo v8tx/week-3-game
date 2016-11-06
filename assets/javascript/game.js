1c7a6a6dc01090e95ce3be0b0b321c295e854851
Veronica Diaz <veronica.diaz@endurance.com>
2016-11-06 00:45:07 -0500

    recreated repo and uploading files

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+<script>
+			var walkingDead = ["rick", "walkers", "carol", "carl", "glenn"];
+
+
+		    for (var i=0; i < walkingDead.length; i++){
+		        document.write(walkingDead[i]);
+		    }
+
+</script>
